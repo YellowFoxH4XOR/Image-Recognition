d88be5e6ec1a2e03111ad53ef41972de4c0771cc
Akshat katiyar <yellowfoxh4xor@gmail.com>
2017-09-21 08:16:25 -0700

    Added image example

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import cv2
 import numpy as np
-MIN_MATCH_COUNT=30
+from matplotlib import pyplot as plt
 
-detector=cv2.SIFT()
+img = cv2.imread('messi5.jpg',0)
+img2 = img.copy()
+template = cv2.imread('template.jpg',0)
+w, h = template.shape[::-1]
 
-FLANN_INDEX_KDITREE=0
-flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
-flann=cv2.FlannBasedMatcher(flannParam,{})
+methods = ['cv2.TM_CCOEFF']
 
-trainImg=cv2.imread("TrainingData/TrainImg.jpeg",0)
-trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
-
-cam=cv2.VideoCapture(0)
-while True:
-    ret, QueryImgBGR=cam.read()
-    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
-    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
-    matches=flann.knnMatch(queryDesc,trainDesc,k=2)
-
-    goodMatch=[]
-    for m,n in matches:
-        if(m.distance<0.75*n.distance):
-            goodMatch.append(m)
-    if(len(goodMatch)>MIN_MATCH_COUNT):
-        tp=[]
-        qp=[]
-        for m in goodMatch:
-            tp.append(trainKP[m.trainIdx].pt)
-            qp.append(queryKP[m.queryIdx].pt)
-        tp,qp=np.float32((tp,qp))
-        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
-        h,w=trainImg.shape
-        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
-        queryBorder=cv2.perspectiveTransform(trainBorder,H)
-        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+    res = cv2.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        top_left = min_loc
     else:
-        print ("Not Enough match found- %d/%d")%(len(goodMatch),MIN_MATCH_COUNT)
-    cv2.imshow('result',QueryImgBGR)
-    if cv2.waitKey(10)==ord('q'):
-        break
-cam.release()
-cv2.destroyAllWindows()
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    cv2.rectangle(img,top_left, bottom_right, 255, 2)
+
+    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+
+    plt.show()
